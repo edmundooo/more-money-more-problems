e912433d8866be3ed4adbbe847cb9ba6477f6ca2
Edmund Chitwood <edmundchitwood@gmail.com>
2018-09-26 18:06:51 -0400

    Made explicit wget Twitter names for each House

--- 01_Get_Congress_Tweets.py
+++ 01_Get_Congress_Tweets.py
@@ -21,30 +21,35 @@ import got
 import csv
 import shutil
 
-# Access CSV file that contains twitter handles of current 
-# Members of the US congressm as of May 23, 2017
-# Source for House of Representatives: 
-# https://gwu-libraries.github.io/sfm-ui/resources/115th-Congress-House-seeds.csv
+# Download CSV file that contains Twitter usernames of current 
+# members of the US Congress as of May 23, 2017
+# House of Representatives: 
+!wget https://gwu-libraries.github.io/sfm-ui/resources/115th-Congress-House-seeds.csv
+with open("115th-Congress-House-seeds.csv") as f:
+        house_twitter_names = [row.split(',')[0] for row in f]
+	
 # Source for Senate: 
-# https://gwu-libraries.github.io/sfm-ui/resources/115th-Congress-Senate-seeds.csv 
-with open("115th-Congress-seeds.csv") as f:
-        twitter_handles = [row.split(',')[0] for row in f]
+!wget https://gwu-libraries.github.io/sfm-ui/resources/115th-Congress-Senate-seeds.csv 
+with open("115th-Congress-Senate-seeds.csv") as f:
+        senate_twitter_names = [row.split(',')[0] for row in f]
+
+twitter_names = house_twitter_names + senate_twitter_names
 
 # Get Tweets and Tweet metadata for each current member 
 # of the US Congress
-for handle in twitter_handles:
+for name in twitter_names:
 
-	tweetCriteria = got.manager.TweetCriteria().setUsername(handle)
+	tweetCriteria = got.manager.TweetCriteria().setUsername(name)
 	tweet = got.manager.TweetManager.getTweets(tweetCriteria)
 
-    # Create a CSV file to hold the Tweets of each 
-    # member and name it based on Twitter handle
-	with open(handle+'_tweets.csv', mode='w') as employee_file:
+    	# Create a CSV file to hold the data for each 
+    	# Congress member and name it based on Twitter handle
+	with open(name+'_tweets.csv', mode='w') as employee_file:
         	employee_writer = csv.writer(employee_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 		for line in tweet:
 			encoded_tweet=line.text.encode('utf-8')
 			employee_writer.writerow([line.username,line.date,line.retweets,line.favorites,encoded_tweet,line.mentions,line.hashtags,line.id,line.permalink])
 
-    # Move each file into data folder
-	shutil.move('/home/ubuntu/data/'+handle+'_tweets.csv', '/home/ubuntu/data/'+handle+'_tweets.csv')
+    	# Move each file into data folder
+	shutil.move('/home/ubuntu/data/'+name+'_tweets.csv', '/home/ubuntu/data/'+name+'_tweets.csv')
